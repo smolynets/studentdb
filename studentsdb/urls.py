fb4d5e14843e1a4036fdf1cae18688f28601f626
Oleh Smolynets <smolynets@gmail.com>
2016-06-24 00:08:52 +0300

    page 239

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include,url
 from django.contrib import admin
+from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = patterns('',
 # Students urls
@@ -35,3 +36,8 @@ url(r'^groups/(?P<gid>\d+)/delete/$',
 name='groups_delete'),
 url(r'^admin/', include(admin.site.urls)),
 )
+if DEBUG:
+ # serve files from media folder
+ urlpatterns += patterns('',
+ url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+ 'document_root': MEDIA_ROOT}))
